6209b18b0cc033d784c503155862e01d0aeab301
Wiktor Wijata <wiktorwijata@gmail.com>
2023-11-09 17:14:37 +0100

    add AggregateRoot

---
+++ RescuePC.Software.CQRS/AggregateRoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using RescuePC.Software.CQRS.Event;
+
+namespace RescuePC.Software.CQRS;
+
+public abstract class AggregateRoot<TKey> : IAggregateRoot
+{
+    public abstract TKey Id { get; protected set; }
+
+    private Queue<IEvent> _eventsQueue = new Queue<IEvent>();
+
+    public virtual void RaiseEvent<TEvent>(TEvent @event) where TEvent : IEvent
+    {
+        if (_eventsQueue == null)
+        {
+            _eventsQueue = new Queue<IEvent>();
+        }
+
+        _eventsQueue.Enqueue(@event);
+    }
+
+    public virtual Queue<IEvent> GetEventsToPublish()
+    {
+        if (_eventsQueue == null)
+        {
+            _eventsQueue = new Queue<IEvent>();
+        }
+
+        return _eventsQueue;
+    }
+}
+
+public abstract class AggregateRoot : AggregateRoot<long> { }
---
+++ RescuePC.Software.CQRS/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using RescuePC.Software.CQRS.Event;
+
+namespace RescuePC.Software.CQRS;
+
+public interface IAggregateRoot
+{
+    void RaiseEvent<TEvent>(TEvent @event) where TEvent : IEvent;
+
+    Queue<IEvent> GetEventsToPublish();
+}
